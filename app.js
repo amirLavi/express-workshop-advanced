a5dc9b384fafc94d65e7662c862c8c249e41fb8d
Amir <al3526@columbia.edu>
2018-01-09 13:13:44 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var app = express();
 var chalk = require('chalk');
-// var routes = require('./routes'); uncomment this when you are ready to connect your router
+var volleyball = require('volleyball');
+var bodyParser = require('body-parser');
+var routes = require('./routes'); //uncomment this when you are ready to connect your router
+
+app.use(volleyball);
+
+app.use(bodyParser());
 
 app.get('/', function (req, res, next) {
   res.send('Root Route');
+  // next();
 });
 
+app.use('/api', routes);
+
+// app.post('/', function (req, res){
+//     console.log('post');
+//     res.send('POST request');
+// });
+//
+// app.get('/', function (req, res){
+//     console.log('get');
+//     // res.send('get request');
+// });
 
 var PORT = 8000;
 
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
 // Where you will connect your other routers
+const express = require('express');
+var router = express.Router();
+var userRouter = require('./user');
+
+// router.use('/users', userRouter);
+// /api/
+router.get('/', function (req, res, next) {
+    res.send('route /api route');
+})
+
+module.exports = router;
